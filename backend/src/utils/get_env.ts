a8a734d3662bfcd7adaebf27f1cade7ac3ada8e5
sejaljain <jsejal74@gmail.com>
2026-07-18 22:14:42 +0530

    Delete backend directory

--- backend/src/utils/get_env.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-export const getEnv = (key: string): string => {
-  const value = process.env[key];
-  if (!value) throw new Error(`Missing ${key}`);
-  return value;
-};
